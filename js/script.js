f3c4a539590fd9dd467d427fe9753d771b244d20
Estefanny Oliveira <estefannyoliveira453@gmail.com>
2024-03-26 23:06:05 -0400

    etapa final

---
+++ js/script.js
@@ -0,0 +1,18 @@
+const navControl = document.getElementById('nav-control');
+const navList = document.querySelector('.nav-list')
+
+function ativarMenu(event) {
+    navList.classList.toggle('ativo');
+}
+navControl.addEventListener('click', ativarMenu);
+
+const navListItens = document.querySelectorAll('.nav-item a');
+function fecharMenu() {
+    navList.classList.remove('ativo')
+}
+
+function cliqueItens(itens) {
+    itens.addEventListener('click', fecharMenu)
+}
+
+navListItens.forEach(cliqueItens)
